ff968d0f5811c3e33057754b4695b70ef4393899
Toretto <saviourchidubem17@gmail.com>
2025-06-18 19:15:14 +0100

    working on dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
     <nav className="bg-white shadow-md sticky top-0 z-50">
@@ -22,32 +23,35 @@ export default function Navbar() {
             Home
           </Link>
 
-          {/* View Journal with Hover Dropdown */}
-          <div className="relative group cursor-pointer">
+          {/* View Journal with hover dropdown wrapped in a container */}
+          <div
+            className="relative cursor-pointer"
+            onMouseEnter={() => setDropdownOpen(true)}
+            onMouseLeave={() => setDropdownOpen(false)}
+          >
             <span className="hover:text-blue-500">View Journal</span>
-            <div className="absolute hidden group-hover:block bg-white shadow-lg rounded-md mt-2 w-52 z-10">
-              <Link to="/about" className="block px-4 py-2 hover:bg-gray-100">
-                About Journal
-              </Link>
-              <Link
-                to="/guidelines"
-                className="block px-4 py-2 hover:bg-gray-100"
-              >
-                Authors' Guidelines
-              </Link>
-              <Link
-                to="/editorial-team"
-                className="block px-4 py-2 hover:bg-gray-100"
-              >
-                Editorial Team
-              </Link>
-              <Link to="/privacy" className="block px-4 py-2 hover:bg-gray-100">
-                Privacy Statement
-              </Link>
-              <Link to="/contact" className="block px-4 py-2 hover:bg-gray-100">
-                Contact
-              </Link>
-            </div>
+            {dropdownOpen && (
+              <div className="absolute top-4 flex flex-col bg-white shadow-lg rounded-md mt-2 w-52 z-10">
+                <Link to="/about" className="px-4 py-2 hover:bg-gray-100">
+                  About Journal
+                </Link>
+                <Link to="/guidelines" className="px-4 py-2 hover:bg-gray-100">
+                  Authors' Guidelines
+                </Link>
+                <Link
+                  to="/editorial-team"
+                  className="px-4 py-2 hover:bg-gray-100"
+                >
+                  Editorial Team
+                </Link>
+                <Link to="/privacy" className="px-4 py-2 hover:bg-gray-100">
+                  Privacy Statement
+                </Link>
+                <Link to="/contact" className="px-4 py-2 hover:bg-gray-100">
+                  Contact
+                </Link>
+              </div>
+            )}
           </div>
 
           <Link to="/board" className="hover:text-blue-500">
@@ -90,14 +94,12 @@ export default function Navbar() {
         </div>
       </div>
 
-      {/* Mobile Menu - NO DROPDOWN HERE */}
+      {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden px-4 pb-4 space-y-2 font-semibold">
           <Link to="/" className="block hover:text-blue-500">
             Home
           </Link>
-
-          {/* All View Journal Links Flat */}
           <Link to="/about" className="block hover:text-blue-500">
             About Journal
           </Link>
@@ -113,7 +115,6 @@ export default function Navbar() {
           <Link to="/contact" className="block hover:text-blue-500">
             Contact
           </Link>
-
           <Link to="/board" className="block hover:text-blue-500">
             Current Issue
           </Link>
