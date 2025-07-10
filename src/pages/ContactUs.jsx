3e9cc38f64c246d6ea46e93a124dc0d41596ccac
Toretto <saviourchidubem17@gmail.com>
2025-07-10 14:03:08 +0100

    just added dark mode in in three other page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,8 +2,8 @@ import AsideSection from "../components/AsideSection";
 
 export default function ContactUs() {
   return (
-    <div className="flex flex-col md:flex-row gap-4 p-4 w-full">
-      <div className="md:w-[80%]  mx-auto md:pl-6 p-6 px-2 text-gray-800">
+    <div className="flex flex-col md:flex-row gap-4 p-4 w-full dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <div className="md:w-[80%]  mx-auto md:pl-6 p-6 px-2 text-gray-800 dark:text-gray-200">
         <h1 className="text-3xl font-bold mb-6 text-green-700">Contact Us</h1>
         <p className="text-lg mb-4">
           For all correspondence, inquiries, and manuscript submissions, please
